ac19487b0074f1180f7228d39d0f5b44e43afcb9
Ray <raynisond@gmail.com>
2022-04-08 14:18:42 +0200

    first commit

---
+++ Assets/Scripts/AudioPeer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPeer : MonoBehaviour
+{
+    public float[] samples = new float[512];
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        GetSpectrumAuDioSource();
+    }
+
+    private void GetSpectrumAuDioSource()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+}
